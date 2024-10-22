2cb3f9b1fa0a23852be18cbdfd0bbe4f98a2f4d9
Umme44 <umme7072@gmail.com>
2024-10-22 04:39:22 -0400

    list

--- src/main/webapp/app/entities/certificate/list/list.component.ts
+++ src/main/webapp/app/entities/certificate/list/list.component.ts
@@ -1,15 +1,194 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute, Data, ParamMap, Router } from '@angular/router';
+import { combineLatest, filter, Observable, switchMap, tap } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ICertificateDetails } from '../certificate.model';
+
+import { ITEMS_PER_PAGE } from 'app/config/pagination.constants';
+import { ASC, DEFAULT_SORT_DATA, DESC, ITEM_DELETED_EVENT, SORT } from 'app/config/navigation.constants';
+import { CertificateService, EntityArrayResponseType } from '../service/certificate.service';
+import { ParseLinks } from 'app/core/util/parse-links.service';
+import { CertificateDelete } from "../delete/delete.component";
+import {IEducationDetails} from "../../education-details/education-details.model";
+
+
+
 @Component({
   selector: 'jhi-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  certificateDetails?: ICertificateDetails[];
+  isLoading = false;
 
-  constructor() { }
+  predicate = 'id';
+  ascending = true;
 
-  ngOnInit(): void {
+  itemsPerPage = ITEMS_PER_PAGE;
+  links: { [key: string]: number } = {
+    last: 0,
+  };
+  page = 1;
+
+  constructor(protected certificateService: CertificateService,
+              protected activatedRoute: ActivatedRoute,
+              public router: Router,
+              protected parseLinks: ParseLinks,
+              protected modalService: NgbModal) {}
+
+  reset(): void {
+    this.page = 1;
+    this.certificateDetails = [];
+    this.load();
   }
 
+  ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+
+
+
+  trackId = (_index: number, item: ICertificateDetails): number => this.certificateService.getCertificateDetailsIdentifier(item);
+
+
+  delete(certificateDetails: ICertificateDetails): void {
+    const modalRef = this.modalService.open(CertificateDelete, {size: 'lg', backdrop: 'static'});
+    modalRef.componentInstance.certificateDetails = certificateDetails;
+    // unsubscribe not needed because closed completes on modal close
+    modalRef.closed
+      .pipe(
+        filter(reason => reason === ITEM_DELETED_EVENT),
+        switchMap(() => this.loadFromBackendWithRouteInformations())
+      )
+      .subscribe({
+        next: (res: EntityArrayResponseType) => {
+          this.onResponseSuccess(res);
+        },
+      });
+  }
+
+
+  protected loadFromBackendWithRouteInformations(): Observable<EntityArrayResponseType> {
+    return combineLatest([this.activatedRoute.queryParamMap, this.activatedRoute.data])
+      .pipe(
+      tap(([params, data]) => this.fillComponentAttributeFromRoute(params, data)),
+      switchMap(() => this.queryBackend(this.page, this.predicate, this.ascending))
+    );
+  }
+
+
+
+  load(): void {
+    this.loadFromBackendWithRouteInformations().subscribe({
+      next: (res: EntityArrayResponseType) => {
+        this.onResponseSuccess(res);
+      },
+    });
+  }
+  navigateToWithComponentValues(): void {
+    this.handleNavigation(this.page, this.predicate, this.ascending);
+  }
+
+  navigateToPage(page = this.page): void {
+    this.handleNavigation(page, this.predicate, this.ascending);
+  }
+
+
+  navigateToPage(page = this.page): void {
+    this.handleNavigation(page, this.predicate, this.ascending);
+  }
+
+  protected loadFromBackendWithRouteInformations(): Observable<EntityArrayResponseType> {
+    return combineLatest([this.activatedRoute.queryParamMap, this.activatedRoute.data]).pipe(
+      tap(([params, data]) => this.fillComponentAttributeFromRoute(params, data)),
+      switchMap(() => this.queryBackend(this.page, this.predicate, this.ascending))
+    );
+  }
+
+  protected fillComponentAttributeFromRoute(params: ParamMap, data: Data): void {
+    const sort = (params.get(SORT) ?? data[DEFAULT_SORT_DATA]).split(',');
+    this.predicate = sort[0];
+    this.ascending = sort[1] === ASC;
+  }
+
+
+  protected onResponseSuccess(response: EntityArrayResponseType): void {
+    this.fillComponentAttributesFromResponseHeader(response.headers);
+    const dataFromBody = this.fillComponentAttributesFromResponseBody(response.body);
+    this.certificateDetails = dataFromBody;
+  }
+
+
+  protected fillComponentAttributesFromResponseBody(data: IEducationDetails[] | null): IEducationDetails[] {
+    const educationDetailsNew = [];
+    if (data) {
+      for (const d of data) {
+        if (educationDetailsNew.map(op => op.id).indexOf(d.id) === -1) {
+          educationDetailsNew.push(d);
+        }
+      }
+    }
+    return educationDetailsNew;
+  }
+
+  protected fillComponentAttributesFromResponseHeader(headers: HttpHeaders): void {
+    const linkHeader = headers.get('link');
+    if (linkHeader) {
+      this.links = this.parseLinks.parse(linkHeader);
+    } else {
+      this.links = {
+        last: 0,
+      };
+    }
+  }
+
+  protected queryBackend(page?: number, predicate?: string, ascending?: boolean): Observable<EntityArrayResponseType> {
+    this.isLoading = true;
+    const pageToLoad: number = page ?? 1;
+    const queryObject = {
+      page: pageToLoad - 1,
+      size: this.itemsPerPage,
+      sort: this.getSortQueryParam(predicate, ascending),
+    };
+    return this.certificateService.query(queryObject).pipe(tap(() => (this.isLoading = false)));
+  }
+
+
+  protected handleNavigation(page = this.page, predicate?: string, ascending?: boolean): void {
+    const queryParamsObj = {
+      page,
+      size: this.itemsPerPage,
+      sort: this.getSortQueryParam(predicate, ascending),
+    };
+    this.router.navigate(['./'], {
+      relativeTo: this.activatedRoute,
+      queryParams: queryParamsObj,
+    });
+  }
+
+  protected getSortQueryParam(predicate = this.predicate, ascending = this.ascending): string[] {
+    const ascendingQueryParam = ascending ? ASC : DESC;
+    if (predicate === '') {
+      return [];
+    } else {
+      return [predicate + ',' + ascendingQueryParam];
+    }
+
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
